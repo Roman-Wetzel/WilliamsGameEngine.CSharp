3e06bc993e99482c7ead3935fc9ba9bf554d72dd
Roman-Wetzel <romanw10@icloud.com>
2026-05-04 08:33:12 -0700

    5/4 stared work on the scrolling background

---
+++ MyGame/ScrollingBackground.cs
@@ -0,0 +1,14 @@
+using GameEngine;
+using SFML.Graphics;
+using SFML.System;
+
+namespace MyGame
+{
+    //please please please background please scroll for me i need you to scroll so i can get an A on this assignment
+    class Background : GameObject
+    {
+        private const float speed = 0.8f;
+       //Background background = new Background();
+    }
+    
+}
